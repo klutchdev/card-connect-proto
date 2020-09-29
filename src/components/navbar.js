1254dcebed29d2706f4bdb472916ab33aa4734b6
KlutchDEV <klutchdevelopment@gmail.com>
2020-09-29 04:23:43 -0400

    oops

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -46,7 +46,7 @@ const StyledMenu = styled.nav`
   width: 90%;
   display: flex;
   flex-direction: column;
-  position: absolute;
+  position: fixed;
   top: 0;
   right: 0;
   margin: 0.5rem 1.75rem;
